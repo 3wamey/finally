6b0f356032725ad33758c8fa3f401e1a8597370d
abobakr.awamey <abm548353@gmail.com>
2024-10-10 18:33:31 +0300

    Comparison

--- src/main/java/org/example/data/Main.java
+++ src/main/java/org/example/data/Main.java
@@ -1,9 +1,44 @@
 package org.example.data;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("hello ");
+
+        /* V. Comparison   */
+
+        Scanner scanner = new Scanner(System.in);
+
+        int A, B;
+
+        char C;
+
+        A = scanner.nextInt();
+        C = scanner.next().charAt(0);
+        B = scanner.nextInt();
+
+
+        if (C == '<') {
+            if (A < B) {
+                System.out.println("Right");
+            } else {
+                System.out.println("Wrong");
+            }
+        } else if (C == '>') {
+            if (A > B) {
+                System.out.println("Right");
+            } else {
+                System.out.println("Wrong");
+            }
+        } else if (C == '=') {
+            if (A == B) {
+                System.out.println("Right");
+            } else {
+                System.out.println("Wrong");
+            }
+        }
+
 
         }
     }
